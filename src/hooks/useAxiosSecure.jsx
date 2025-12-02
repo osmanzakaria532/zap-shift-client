6b3b1209b6de902040b94b34eff80427f55d01c8
osmanzakaria532 <osmanzakaria179@gmail.com>
2025-12-02 12:02:32 +0600

    change localURL with serverURL in axios

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router';
 import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://zap-shift-server-osmanzakaria.vercel.app',
 });
 
 const useAxiosSecure = () => {
