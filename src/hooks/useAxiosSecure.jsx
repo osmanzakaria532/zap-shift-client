e2b5346f19e7aa32de4e45ee64218fb92e2fd3ae
osmanzakaria532 <osmanzakaria179@gmail.com>
2026-01-22 19:25:41 +0600

    in the main.jsx off StricMode and added interceptors for traching logged user

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,52 @@
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import useAuth from './useAuth';
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 const useAxiosSecure = () => {
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Add a request interceptor
+    const requestInterceptor = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+
+    // Response the interceptor when the component unmounts
+    const responseInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log(error);
+
+        const statusCode = error.status;
+        if (statusCode === 401 || statusCode === 403) {
+          // Handle unauthorized or forbidden responses
+          logOut()
+            .then(() => {
+              console.log('Logged out due to unauthorized access');
+              navigate('/sign-in');
+            })
+            .catch((err) => {
+              console.error('Error during logout:', err);
+            });
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(requestInterceptor);
+      axiosSecure.interceptors.response.eject(responseInterceptor);
+    };
+  }, [user, logOut, navigate]);
   return axiosSecure;
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import AuthProvider from './Contexts/AuthContext/AuthProvider.jsx';
@@ -9,11 +8,10 @@ import router from './routes/router.jsx';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-    </QueryClientProvider>
-  </StrictMode>,
+  // <StrictMode></StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </QueryClientProvider>,
 );
